3fb3ada6a9dfa7cdb2c2d2e9d79085e8358aa220
Teo-Sav <mateo.saavedrar.est@uets.ecu.ec>
2023-11-17 13:51:04 -0500

    v2

--- src/app/carrito/carrito.page.ts
+++ src/app/carrito/carrito.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.page.html',
   styleUrls: ['./carrito.page.scss'],
 })
-export class CarritoPage implements OnInit {
+export class CarritoPage {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {}
+  goHome(){
+    this.router.navigate(['/home']);
   }
 
 }
